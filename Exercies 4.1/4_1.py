955104e6aa0344ee6d36674ac3f373776352d965
klutten99 <klutten99@gmail.com>
2021-11-04 00:19:23 +0100

    Update to rules

--- Exercies 4.1/4_1.py
+++ Exercies 4.1/4_1.py
@@ -1,32 +1,47 @@
 # 000 001 ... 110 111
-rule_90 = [0, 1, 0, 1, 1, 0, 1, 0]
-rule_30 = [0, 1, 1, 1, 1, 0, 0, 0]
-rule_110 = [0, 1, 1, 1, 0, 1, 1, 0]
-rule_184 = [0, 0, 0, 1, 1, 1, 0, 1]
+import numpy as np
+import imagesc as imagesc
+import time
+
+# Predefined rules
+# cellular automata that evolve towardshomogeneity (class 1),
+# that evolve towards an oscillating or periodic condition (class 2),
+# that display chaotic behaviors (class 3),
+# and that display complex emergent behaviors (class 4).
+rule_90 = [0, 1, 0, 1, 1, 0, 1, 0]  # class-4
+rule_30 = [0, 1, 1, 1, 1, 0, 0, 0]  # class-3
+rule_110 = [0, 1, 1, 1, 0, 1, 1, 0]  # class-4/3
+rule_184 = [0, 0, 0, 1, 1, 1, 0, 1]  # class-2
 
 
 def get_next_value(rule, state):
-    return rule[int(''.join(map(str, state)), 2)]
+    return rule[int("".join(map(str, state)), 2)]
 
 
 def get_next_generation(rule, generation):
     next_generation = []
 
-    state = [generation[-1]]+generation[0:2]  # Special case for first
+    state = [generation[-1]] + generation[0:2]  # Special case for first
     next_generation.append(get_next_value(rule, state))
 
-    for i in range(1, len(generation)-1):
-        state = generation[i-1:i+2]
+    for i in range(1, len(generation) - 1):
+        state = generation[i - 1 : i + 2]
         next_generation.append(get_next_value(rule, state))
 
-    state = generation[i:i+2] + [generation[0]]  # Special case for last
+    state = generation[i : i + 2] + [generation[0]]  # Special case for last
     next_generation.append(get_next_value(rule, state))
     return next_generation
 
 
-generation = [0]*30+[1]+[0]*30
-n_generations = 6
+# Rule 90/30
+# generation = [0] * 30 + [1] + [0] * 30
+# Rule 110/184
+generation = np.random.randint(0, 2, size=(61)).tolist()
+n_generations = 32
+generations = []
 
-for gen in range(n_generations):
-    print(generation)
-    generation = get_next_generation(rule_90, generation)
+for i in range(n_generations):
+    generations.append(generation)
+    generation = get_next_generation(rule_110, generation)
+
+imagesc.plot(np.array(generations))
