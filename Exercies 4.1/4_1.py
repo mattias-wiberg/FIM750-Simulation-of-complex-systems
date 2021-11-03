8188887404e9a233bcb83373e4780d2c7fb22fde
klutten99 <klutten99@gmail.com>
2021-11-03 23:18:30 +0100

    Update 4_1.py

--- Exercies 4.1/4_1.py
+++ Exercies 4.1/4_1.py
@@ -1,22 +1,32 @@
 # 000 001 ... 110 111
-rule_90 = [0,1,0,1,1,0,1,0]
-rule_30 = [0,1,1,1,1,0,0,0]
-rule_110 = [0,1,1,1,0,1,1,0]
-rule_184 = [0,0,0,1,1,1,0,1]
+rule_90 = [0, 1, 0, 1, 1, 0, 1, 0]
+rule_30 = [0, 1, 1, 1, 1, 0, 0, 0]
+rule_110 = [0, 1, 1, 1, 0, 1, 1, 0]
+rule_184 = [0, 0, 0, 1, 1, 1, 0, 1]
+
 
 def get_next_value(rule, state):
-    return rule[int(''.join(map(str, state)),2)] # Middle element
+    return rule[int(''.join(map(str, state)), 2)]
+
 
 def get_next_generation(rule, generation):
     next_generation = []
-    for i in range(len(generation)-1):
-        state = generation[i-1:i+1]
+
+    state = [generation[-1]]+generation[0:2]  # Special case for first
+    next_generation.append(get_next_value(rule, state))
+
+    for i in range(1, len(generation)-1):
+        state = generation[i-1:i+2]
         next_generation.append(get_next_value(rule, state))
 
-    state = generation[i:i+1] + generation[0] # Special case for last
+    state = generation[i:i+2] + [generation[0]]  # Special case for last
     next_generation.append(get_next_value(rule, state))
     return next_generation
-    
 
-generation = [0,0,0,1,0,0,0]
-get_next_generation(rule_90, )
+
+generation = [0]*30+[1]+[0]*30
+n_generations = 6
+
+for gen in range(n_generations):
+    print(generation)
+    generation = get_next_generation(rule_90, generation)
