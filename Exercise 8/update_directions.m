b6dbb4d8352528c02372b25251f9c3e90963874d
Mattias <klutten99@gmail.com>
2021-11-14 21:31:06 +0100

    Update update_directions.m
    Tested a bit and fixed some errors should be working now

--- Exercise 8/update_directions.m
+++ Exercise 8/update_directions.m
@@ -1,15 +1,20 @@
-function directions = update_directions(particles, Rf, dt)
-    directions = zeros(size(particles));
-    for j = 1:size(particles)
+function directions = update_directions(particles, L, Rf, eta, dt)
+    nParticles = size(particles, 1);
+    directions = zeros(nParticles,1);
+    for j = 1:nParticles
         % calc differance on all particles except current one
         differance = abs(particles(setdiff(1:end, j),1:2) - particles(j, 1:2));
         % Wrap around distance when over L/2
         differance(differance > L/2) = L - differance(differance > L/2); 
         
         in_range = sqrt(sum(differance.^2, 2)) < Rf;
-        avg = mean(particles(in_range, 3));
         Wn = randn();
-        directions(j) = avg + eta*Wn*dt;
+        if sum(in_range) > 0
+            avg = mean(particles(in_range, 3));
+            directions(j) = avg + eta*Wn*dt;
+        else
+            directions(j) = eta*Wn*dt;
+        end
     end
 end
 
