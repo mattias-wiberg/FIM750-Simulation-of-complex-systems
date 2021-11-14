d903254e914cc3f11fbf6116d04e409813f87e17
Mattias <klutten99@gmail.com>
2021-11-14 21:21:12 +0100

    Ignore

--- Exercise 8/update_particles.m
+++ Exercise 8/update_particles.m
@@ -1,8 +1,5 @@
 function particles = update_particles(particles, Rf)
-%UPDATE_PARTICLES Summary of this function goes here
-%   Detailed explanation goes here
-    particles = 
-    particles = update_directons(particles)
+    particles(:,3) = update_directons(particles);
     particles = update_positions(particles)
     
 end
