0a0cb76cfadaf761658ccf1ce156f218960d5780
Mattias <klutten99@gmail.com>
2021-11-15 17:04:51 +0100

    Clean up

--- Exercise 8/model.m
+++ Exercise 8/model.m
@@ -5,7 +5,7 @@ clear;
 N = 100;
 L = 100;
 rho = N/L^2; % density rho = N/L^2
-eta = 0.1; % Noise
+eta = 0; % Noise
 Rf = 1; % Interaction radius radius
 dt = 1;
 v = 1;
@@ -33,7 +33,7 @@ for i = 1:S
     end
     
     if mod(i, 1000)==1
-        disp(i);
+        fprintf("%d\n",i);
     end
 end
 
@@ -42,45 +42,15 @@ clf
 subplot(1,3,1)
 voronoi(inital_particles(:,1), inital_particles(:,2));
 pbaspect([1 1 1])
-ylim([-L/2 L/2])
-xlim([-L/2 L/2])
 subplot(1,3,2)
 voronoi(particles(:,1), particles(:,2))
 pbaspect([1 1 1])
-ylim([-L/2 L/2])
-xlim([-L/2 L/2])
 subplot(1,3,3)
 plot(1:S, g_align);
 hold on
 plot(1:S, g_clust);
 ylim([0 1.1])
-legend('\Psi', 'c')
+legend('\Psi', 'c', 'Location', 'southeast')
 xlabel('t')
 ylabel('\Psi, c')
 pbaspect([1 1 1])
-%% Voronoi correct?
-M = [
-    particles(:,1) particles(:,2)
-    particles(:,1)-L particles(:,2)-L
-    particles(:,1)-L particles(:,2)
-    particles(:,1)-L particles(:,2)+L
-    particles(:,1) particles(:,2)-L
-    particles(:,1) particles(:,2)+L
-    particles(:,1)+L particles(:,2)-L
-    particles(:,1)+L particles(:,2)
-    particles(:,1)+L particles(:,2)+L];
-voronoi(M(:,1), M(:,2))
-grid on
-xticks([-L L]./2)
-yticks([-L L]./2)
-[v, c] = voronoin(M(:,1:2));
-total_a = 0;
-for i = 1:100
-    v1 = v(c{i},1);
-    v2 = v(c{i},2);
-    patch(v1, v2, 'C')
-    total_a = total_a + polyarea(v1,v2);
-end
-total_a
-[v, c] = voronoin(particles(:,1:2));
-(total_a-nansum(get_voronoi_areas(v, c)))/total_a
--- Exercise 8/update_directions.m
+++ Exercise 8/update_directions.m
@@ -1,7 +1,6 @@
 function directions = update_directions(particles, L, Rf, eta, dt)
     nParticles = size(particles, 1);
     directions = zeros(nParticles,1);
-    Wn = randn();
     for j = 1:nParticles
         % calc differance on all particles except current one
         differance = abs(particles(:,1:2) - particles(j, 1:2));
@@ -11,6 +10,7 @@ function directions = update_directions(particles, L, Rf, eta, dt)
         in_range = sqrt(sum(differance.^2, 2)) < Rf;
         % Circular mean https://en.wikipedia.org/wiki/Circular_mean
         avg = atan2(sum(sin(particles(in_range, 3))), sum(cos(particles(in_range, 3))));
+        Wn = randn();
         directions(j) = avg + eta*Wn*dt;
     end
 end
