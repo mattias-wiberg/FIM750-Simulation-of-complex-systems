37603be8c1dc8ee9f3f86b40250a603e4907cc7e
Mattias <klutten99@gmail.com>
2021-11-17 14:24:18 +0100

    Images for 7 and 8

--- Exercise 8/model.m
+++ Exercise 8/model.m
@@ -3,13 +3,14 @@ clear;
 
 % Constants
 N = 100;
-L = 100;
+L = 1000;
 rho = N/L^2; % density rho = N/L^2
-eta = 0.1; % Noise
-Rf = 1; % Interaction radius radius
+eta = 0.4; % Noise
+Rf = 20; % Interaction radius radius
 dt = 1;
-v = 1;
+v = 3;
 S = 10^4; % Number of interations
+h = 1;
 
 g_align = zeros(S, 1);
 g_clust = zeros(S, 1);
@@ -17,14 +18,18 @@ g_clust = zeros(S, 1);
 % Particles
 inital_particles = initilize_particles(N, L, v);
 %% Run
+g_a_hist = [];
+g_c_hist = [];
+for h = 25
+    h
 history = zeros(N, 4, S);
 particles = inital_particles;
 j = 1;
 for i = 1:S
-    %history(:,:,i) = particles;
+    history(:,:,i) = particles;
     g_align(i) = alignment_coefficient(particles, v);
     g_clust(i) = global_clustering_coeff(particles, Rf, L);
-    particles = update_particles(particles, L, Rf, eta, dt);
+    particles = update_particles(particles, L, Rf, eta, dt, history, h, i);
     if any(i == [10 100 500 1000])
         subplot(1, 4, j)
         voronoi(particles(:,1), particles(:,2))
@@ -44,8 +49,10 @@ for i = 1:S
         fprintf("%d\n",i);
     end
 end
-%saveas(gcf, strcat('./Images/7/rf_',int2str(Rf),'_steps.png'))
-%% Plotting
+saveas(gcf, strcat('./Images/8/h_',int2str(h),'_steps.png'))
+g_a_hist = [g_a_hist g_align];
+g_c_hist = [g_c_hist g_clust];
+
 clf
 subplot(1,3,1)
 voronoi(inital_particles(:,1), inital_particles(:,2));
@@ -66,4 +73,11 @@ legend('\Psi', 'c', 'Location', 'southeast')
 xlabel('t')
 ylabel('\Psi, c')
 pbaspect([1 1 1])
-%saveas(gcf, strcat('./Images/7/rf_',int2str(Rf),'.png'))
+saveas(gcf, strcat('./Images/8/h_',int2str(h),'.png'))
+end
+%%
+clf
+subplot(1,2,1)
+plot(1:S, [g_a_hist(:,1) g_a_hist(:,2) g_a_hist(:,26)])
+subplot(1,2,2)
+plot(1:S, [g_c_hist(:,1) g_c_hist(:,2) g_c_hist(:,26)])
--- Exercise 8/update_particles.m
+++ Exercise 8/update_particles.m
@@ -1,6 +1,24 @@
-function particles = update_particles(particles, L, Rf, eta, dt)
+function particles = update_particles(particles, L, Rf, eta, dt, history, h, n)
+    s = 10;
     directions = update_directions(particles, L, Rf, eta, dt);
-    particles(:, 1:2) = update_positions(particles, L, dt);
-    particles(:, 3) = directions;
+    positions = update_positions(particles, L, dt);
+    
+    if h > 0 && n > h
+        particles(:, 1:2) = positions;
+        particles(:, 3) = update_directions(history(:,:,n-h), L, Rf, eta, dt);
+    elseif h < 0 && n > s
+        N = size(particles, 1);
+        future_directions = zeros(N, 1);
+        for i = 1:N
+            p = polyfit(1:s, history(i, 3, n-s:n-1), 1);
+            future_directions(i) = polyval(p, s+abs(h));
+        end
+        particles(:, 3) = future_directions;
+        particles(:, 1:2) = update_positions(particles, L, dt);
+        particles(:, 3) = directions;
+    else
+        particles(:, 1:2) = positions;
+        particles(:, 3) = directions;
+    end
 end
 
