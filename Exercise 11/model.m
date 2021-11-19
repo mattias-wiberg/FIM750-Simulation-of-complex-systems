6414d6361de530e51b80033c445fb931d988df19
Mattias <klutten99@gmail.com>
2021-11-19 13:07:47 +0100

    Added some plots

--- Exercise 11/model.m
+++ Exercise 11/model.m
@@ -1,7 +1,7 @@
 clc;
 clear;
 N = 1000;
-beta = 0.4;
+beta = 0.1;
 gamma = 0.01;
 ifr = 0.01; % Init infection rate
 k = beta/gamma;
@@ -9,6 +9,7 @@ d = 0.8; % Diffusion rate
 L = 100; % Lattice size
 
 % S = 1, I = 2, R = 3
+for beta = linspace(0, 1, 40)
 world = init_world(L, N, ifr);
 t = 0;
 S = [];
@@ -19,25 +20,25 @@ while ~isempty(find(cellfun(@(x) any(x==2), world), 1))
     world = infect(world, beta);
     world = recover(world, gamma);
     t = t + 1
-    S = [S sum(cellfun(@(x) sum(x==1), world), 'all')];
-    I = [I sum(cellfun(@(x) sum(x==2), world), 'all')];
-    R = [R sum(cellfun(@(x) sum(x==3), world), 'all')];
+    %S = [S sum(cellfun(@(x) sum(x==1), world), 'all')];
+    %I = [I sum(cellfun(@(x) sum(x==2), world), 'all')];
+    %R = [R sum(cellfun(@(x) sum(x==3), world), 'all')];
     %show_world(world);
     %title(strcat('t = ', int2str(t)))
     %pause(0.01)
 end
-%%
+
 clf;
+subplot(1,2,1)
+show_world(world);
+title(strcat('t = ', int2str(t)))
+subplot(1,2,2)
 hold on;
 plot(1:t, S, 'Color', 'b')
 plot(1:t, I, 'Color', '#FFA500')
 plot(1:t, R, 'Color', 'g')
 legend('S', 'I', 'R')
-%%
-nS = 0;
-nR = 0;
-for i = 1:numel(world) % col by col (x by x) (y,x)
-    nS = nS + sum(world{i}==1);
-    nR = nR + sum(world{i}==3);
+SIR = [S(end) I(end) R(end)];
+saveas(gcf, strcat('./1/beta_', num2str(beta), '.png'))
+save(strcat('./1/beta_', num2str(beta), '.mat'))
 end
-nS,nR
--- Exercise 11/show_world.m
+++ Exercise 11/show_world.m
@@ -1,5 +1,6 @@
 function fig = show_world(world)
     scatter = cellfun(@mode, world); % Most occurences
+    %scatter = cellfun(@mean, world); % Mean of cell agent types
     scatter(isnan(scatter)) = 0;
     map = [
         1 1 1
