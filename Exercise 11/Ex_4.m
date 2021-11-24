942694c30ecf7219e14333cf1a09e39e423c8d53
klutten99 <klutten99@gmail.com>
2021-11-24 22:21:00 +0100

    Ex4 fixed

--- Exercise 11/Ex_4.m
+++ Exercise 11/Ex_4.m
@@ -13,21 +13,24 @@ alpha = 0.01;
 
 % S = 1, I = 2, R = 3
 averages = 3;
-steps = 40;
-vars = 6;
+steps = 20;
+vars = 7;
 SIRt = zeros(steps, vars, averages);
 i = 1;
-for k = linspace(2, 80, steps) %beta/gamma
-    for beta = linspace(0.01, 1, steps)
-        beta
-        gamma = beta/k
-        for avg = 1:averages
-            avg
-            clf;
-            [S, I, R, t] = simulation(L, N, ifr, d, beta, gamma, mu, alpha);
-            SIRt(i, :, avg) = [beta, gamma, S(end), I(end), R(end), t];
+for alpha = 0.2
+    alpha
+    for gamma = linspace(0.01, 1, steps) 
+        gamma
+        for beta = linspace(0.01, 1, steps)
+            beta
+            for avg = 1:averages
+                avg
+                clf;
+                [S, I, R, t] = simulation(L, N, ifr, d, beta, gamma, mu, alpha);
+                SIRt(i, :, avg) = [alpha, beta, gamma, S(end), I(end), R(end), t];
+            end
+            i = i + 1;
         end
-        i = i + 1;
     end
 end
 save('SIRt.mat', 'SIRt')
