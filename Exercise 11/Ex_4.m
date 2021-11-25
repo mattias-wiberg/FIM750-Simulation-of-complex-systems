78a477e6336d59633bd3e3ed7438fa9f0cde73a1
klutten99 <klutten99@gmail.com>
2021-11-25 01:08:13 +0100

    Pictures!

--- Exercise 11/Ex_4.m
+++ Exercise 11/Ex_4.m
@@ -2,35 +2,38 @@ clc;
 clf;
 clear;
 N = 1000;
-beta = 1;
-gamma = 0.00001;
-ifr = 0.01; % Init infection rate
-mu = 0.003;
+beta = 0.8;
+gamma = 0.02;
+ifr = 0.7; % Init infection rate
 k = beta/gamma;
 d = 0.8; % Diffusion rate
 L = 100; % Lattice size
+mu = 1;
 alpha = 0.01;
 
 % S = 1, I = 2, R = 3
-averages = 3;
+averages = 1;
 steps = 20;
 vars = 7;
 SIRt = zeros(steps, vars, averages);
 i = 1;
-for alpha = 0.2
+for alpha = 0.01
     alpha
-    for gamma = linspace(0.01, 1, steps) 
-        gamma
-        for beta = linspace(0.01, 1, steps)
-            beta
-            for avg = 1:averages
-                avg
-                clf;
-                [S, I, R, t] = simulation(L, N, ifr, d, beta, gamma, mu, alpha);
-                SIRt(i, :, avg) = [alpha, beta, gamma, S(end), I(end), R(end), t];
-            end
-            i = i + 1;
-        end
+    for avg = 1:averages
+        avg
+        clf;
+        [S, I, R, t] = simulation(L, N, ifr, d, beta, gamma, mu, alpha);
+        SIRt(i, :, avg) = [alpha, beta, gamma, S(end), I(end), R(end), t];
     end
+    i = i + 1;
 end
-save('SIRt.mat', 'SIRt')
+clf;
+hold on;
+plot(1:t, S, 'Color', 'b')
+plot(1:t, I, 'Color', '#FFA500')
+plot(1:t, R, 'Color', 'g')
+title(strcat('ifr = ', num2str(ifr,5),' \beta = ', num2str(beta,5),' \gamma = ', num2str(gamma,5),' \alpha = ', num2str(alpha,5)))
+pbaspect([1 1 1])
+legend('S', 'I', 'R')
+saveas(gcf, strcat('./4/',num2str(beta,5),'_g_','b_', num2str(beta,5),'_g_', num2str(gamma,5),'_a_', num2str(alpha,5),'.png'));
+%save('SIRt.mat', 'SIRt')
--- Exercise 11/simulation.m
+++ Exercise 11/simulation.m
@@ -8,14 +8,15 @@ function [S, I, R, t] = simulation(L, N, ifr, d, beta, gamma, mu, alpha)
         world = diffuse(world, L, d);
         world = infect(world, beta);
         world = recover(world, gamma);
-        world = eliminate(world, mu); % Exercise 3
-        %world = demmune(world, alpha); % Exercise 4
+        %world = eliminate(world, mu); % Exercise 3
+        world = demmune(world, alpha); % Exercise 4
         t = t + 1;
         [nS, nI, nR] = get_SIR(world);
         S = [S nS];
         I = [I nI];
         R = [R nR];
-        if mod(t,25) == 0
+        if t == 6000
+            return
             %show_world(world);
             %title(strcat('t = ', int2str(t)))
             %pause(0.01)
