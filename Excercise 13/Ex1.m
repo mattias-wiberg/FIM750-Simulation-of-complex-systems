69fe35de5d94aa0fbfb49ea3b1a7a5d0f21b2c53
Mattias <klutten99@gmail.com>
2021-11-30 14:39:38 +0100

    Ex1 Done.

--- Excercise 13/Ex1.m
+++ Excercise 13/Ex1.m
@@ -1,5 +1,7 @@
+%% a.1
 clc
 clear
+clf
 
 N = 10;
 T = 0;
@@ -7,8 +9,7 @@ R = 0.5;
 P = 1;
 S = 1.5;
 game = Game(N,T,R,P,S);
-%% a
-clf
+
 years = zeros(1, N);
 m = 6;
 for i = 1:N+1
@@ -19,8 +20,19 @@ scatter(0:N, years, 'f')
 xlim([0 10])
 ylim([5 11])
 xlabel("n")
-ylabel("Years in rison")
+ylabel("Years in prison")
 %% b
+clc
+clear
+clf
+
+N = 10;
+T = 0;
+R = 0.5;
+P = 1;
+S = 1.5;
+game = Game(N,T,R,P,S);
+
 M = zeros(N+1,N+1);
 for i = 1:size(M,1)
     for j = 1:size(M,2)
@@ -34,29 +46,39 @@ colorbar
 xlabel("m")
 ylabel("n")
 set(gca, 'YDir', 'Normal')
-%%
+%% c
 clc
-m = 6;
-n = 1;
-A = [ones(1, n), zeros(1, N-n)];
-B = [ones(1, m), zeros(1, N-m)];
-yearsA = 0;
-yearsB = 0;
-for i = 1:length(A)
-    if A(i) + B(i) == 2
-        yearsA = yearsA + R;
-        yearsB = yearsB + R;
-    elseif A(i) + B(i) == 0
-        yearsA = yearsA + P;
-        yearsB = yearsB + P;
-    elseif A(i) == 1
-        yearsA = yearsA + S;
-        yearsB = yearsB + T;
-    elseif B(i) == 1
-        yearsA = yearsA + T;
-        yearsB = yearsB + S;
+clear
+clf
+
+N = 10;
+T = 0;
+P = 1;
+m = 8;
+steps = 10;
+nBest = [];
+
+i = 1;
+for R = linspace(T+0.01,P-0.01,steps)
+    j = 1;
+    for S = linspace(P,10,steps)
+        fprintf("%d, %d\n",R,S)
+        clf
+        game = Game(N,T,R,P,S);
+        years = zeros(1, N);
+        for k = 1:N+1
+            n = k - 1;
+            years(k) = game.get_years(n,m);
+        end
+        [~,I] = min(years);
+        nBest(i,j) = I-1;
+        %scatter(0:N, years, 'f')
+        %title(strcat("Best n = ",int2str(I-1), ", (R,S) = (",num2str(R),', ',num2str(S), ')'))
+        %xlabel("n")
+        %ylabel("Years in prison")
+        %pause(0.01)
+        j = j + 1;
     end
+    i = i +1;
 end
-
-
-
+nBest
--- Excercise 13/README.md
+++ Excercise 13/README.md
@@ -1,5 +1,20 @@
 # Exercise 1
-
+## A
+Fixing the opponent's strategy to m shows that the best strategy is n=m-1 
+this can be seen for the case of m=6 as shown below. Although for m = 0 
+n = m should be choosen.
+![alt text](?raw=true)
+## B
+A 2D map of years in prison as a function of the player’s strategy n and 
+the opponent’s strategy m can be seen below.
+![alt text](?raw=true)
+## C
+To check if R and S has any affect on the best strategy n=m-1 the same plot 
+was generated as in Figure 1 and the value of n giving the lowest amount of 
+years was recorded while varying R and S. Having m = 6 and varying R and S 
+gave the following gif.
+![alt text](?raw=true)
+The test was done for all other m and the best strategry was always n=m-1.
 # Exercise 2
 
 # Exercise 3
